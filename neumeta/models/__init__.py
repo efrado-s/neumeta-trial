41e03f7167c0a95a9470fb24b244573f950c3680
efrado <58604575+efrado-s@users.noreply.github.com>
2025-05-14 02:13:48 +0700

    Add __init__.py in models for initializing models

---
+++ neumeta/models/__init__.py
@@ -0,0 +1,44 @@
+import os
+
+import torch
+
+from .resnet_cifar import *
+from .utils import fuse_module
+from smooth.permute import PermutationManager, compute_tv_loss_for_network
+
+def create_model_cifar10(model_name, hidden_dim, path=None, smooth=False):
+    """
+    Create a model based on the specified name.
+
+    :param model_name: String that specifies the model to use.
+    :param path: Optional path for the model's weights.
+    :return: The initialized model.
+    """
+    if model_name == 'ResNet20': # Add other models as you support them
+        model = cifar10_resnet20(hidden_dim=hidden_dim)
+    else:
+        raise ValueError(f'Unsupported model: {model_name}')
+
+    fuse_module(model)  # Fuse convolutional and batch normalization layers
+
+    if path:
+        if os.path.exists(path):
+            print('Loading model from', path)
+            state_dict = torch.load(path, map_location=torch.device('cpu'))
+            load_checkpoint(model, state_dict)
+
+    # Smooth initial weights manifold before processing it
+    if smooth:
+        print('Smooth the parameters of the model')
+        print(f'TV original model: {compute_tv_loss_for_network(model, lambda_tv=1.0).item()}')
+        input_tensor = torch.randn(1, 3, 32, 32)
+        permute_func = PermutationManager(model, input_tensor)
+        permute_dict = permute_func.compute_permute_dict()
+        model = permute_func.apply_permutations(permute_dict, ignored_keys=[
+            ('conv1.weight', 'in_channels'),
+            ('fc.weight', 'out_channels'),
+            ('fc.bias', 'out_channels')
+        ])
+        print(f'TV original model: {compute_tv_loss_for_network(model, lambda_tv=1.0).item()}')
+    
+    return model
