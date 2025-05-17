49027614dc386798e085b4b811c190f5987c8240
efrado <58604575+efrado-s@users.noreply.github.com>
2025-05-17 07:52:18 +0700

    Add functions to select optimizer, and learning_rate stepper and another function to calculate reconstruct loss

--- neumeta/utils/hypernet_utils.py
+++ neumeta/utils/hypernet_utils.py
@@ -141,8 +141,84 @@ def shuffle_coordinates_all(dim_dict):
 
 
 # Function to create optimizer
+def get_optimizer(args, hyper_model):
+    criterion = torch.nn.CrossEntropyLoss()
+    val_criterion = torch.nn.CrossEntropyLoss()
 
-# Function to find weight loss
+    # Get optimizer for training
+    optimizer_name = args.training.get('optimizer', 'adamw')
+    if optimizer_name == 'adamw':
+        optimizer = AdamW(hyper_model.parameters(),
+                          lr=args.training.learning_rate,
+                          weight_decay=args.training.weight_decay)
+    elif optimizer_name == 'adam':
+        optimizer = Adam(hyper_model.parameters(),
+                         lr=args.training.learning_rate,
+                         weight_decay=args.training.weight_decay)
+    elif optimizer_name == 'sgd':
+        optimizer = torch.optim.SGD(hyper_model.parameters(),
+                                    lr=args.training.learning_rate,
+                                    momentum=args.training.get('momentum', 0.9),
+                                    weight_decay=args.training.weight_decay)
+    elif optimizer_name == 'rmsprop':
+        optimizer = torch.optim.RMSprop(hyper_model.parameters(),
+                                        lr=args.training.learning_rate,
+                                        momentum=args.training.get('momentum', 0.9),
+                                        weight_decay=args.training.weight_decay)
+    elif optimizer_name == 'adagrad':
+        optimizer = torch.optim.Adagrad(hyper_model.parameters(),
+                                        lr=args.training.learning_rate,
+                                        weight_decay=args.training.weight_decay)
+    else:
+        raise ValueError(f'Unknown optimizer name: {optimizer_name}')
+    
+    # Decides which step scheduler to use
+    scheduler_name = args.training.get('scheduler', 'multistep')
+    if scheduler_name == 'cosine':
+        print(f'Using cosine scheduler, T_max: {args.training.T_max}')
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.training.T_max)
+    elif scheduler_name == 'multistep':
+        scheduler = MultiStepLR(optimizer,
+                                milestones=args.training.get('lr_steps', [args.experiment.num_epochs]),
+                                gamma=0.1)
+
+    return criterion, val_criterion, optimizer, scheduler
+
+
+# Function to find reconstruct_loss
+def weighted_regression_loss(reconstructed_weights, gt_selected_weights, epsilon=1e-6):
+    """
+    Calculate reconstruct loss by calculating weighted regression loss, with each element in the ground truth weights
+    contributing a weight proportional to its absolute value.
+
+    Args:
+    - reconstructed_weights (list of Tensors): List of newly reconstructed weight tensors.
+    - gt_selected_weights (list of Tensors): List of original weights from pretrained network; ground truth weight tensors.
+    Returns:
+    - torch.Tensor: The mean of the weighted regression losses.
+    """
+    # Initialize an empty list to store individual loss
+    losses = []
+
+    # Iterate over the pairs of reconstructed and ground truth weights
+    for w, w_gt in zip(reconstructed_weights, gt_selected_weights):
+        # Calculate the weights as the absolute values of the ground truth weights
+        element_weights = torch.abs(w_gt) + epsilon
+        element_weights = element_weights / torch.max(element_weights)
+
+        # Compute the MSE loss element-wise
+        element_loss = (w - w_gt) ** 2
+
+        # Apply the weights to the loss
+        weighted_loss = element_loss * element_weights
+
+        # Sum the weighted loss for the current pair and add it to the list
+        losses.append(weighted_loss.mean())
+
+    # Calculate the mean of the weighted losses
+    reconstruct_loss = torch.mean(torch.stack(losses))
+
+    return reconstruct_loss
 
 # Functions to create hypernetwork and using it
 
