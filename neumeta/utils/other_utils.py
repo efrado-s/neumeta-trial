bbc7ce08f5d986a7e4cf8634fe021cf7ab002cc6
efrado <58604575+efrado-s@users.noreply.github.com>
2025-05-17 16:49:17 +0700

    Add a function to set seeds for reproducibility

--- neumeta/utils/other_utils.py
+++ neumeta/utils/other_utils.py
@@ -1,10 +1,37 @@
+import random
 
+import torch
+import numpy as np
 
 # Functions to handle (or parse) arguments
 
 
 # Functions to handle random seed
+def set_seed(seed_value=42):
+    """Set the seed for generating random numbers for PyTorch and other libraries to ensure reproducibility.
 
+    Args:
+        seed_value (int, optional): The seed value. Defaults to 42 (a commonly used value in randomized algorithms requiring a seed).
+    """
+    print(f'Setting seed... {seed_value} for reproducibility')
+    # Set the seed for generating random numbers in Python's random library.
+    random.seed(seed_value)
+
+    # Set the seed for generating random numbers in NumPy, which can also affect randomness in cases where PyTorch relies on NumPy.
+    np.random.seed(seed_value)
+
+    # Set the seed for generating random numbers in PyTorch. This affects the randomness of various PyTorch functions and classes.
+    torch.manual_seed(seed_value)
+
+    # If you are using CUDA, and want to generate random numbers on the GPU, you need to set the seed for CUDA as well.
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed_value)
+        # For multi-GPU, if you are using more than one GPU.
+        torch.cuda.manual_seed_all(seed_value)
+
+        # Additionally, for even more deterministic behavior, you might need to set the following environment, though it may slow down the performance.
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
 
 # Calculating average
